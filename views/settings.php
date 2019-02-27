b8a22d5b6ae3ddc9c49c9f74521fbc5359fc04ea
Dmitry Kozhanov <Medic8463@gmail.com>
2019-02-27 14:22:46 +0400

    [EC-216] Change layout

--- views/settings.php
+++ views/settings.php
@@ -1,27 +1,26 @@
 <div class="smartcat-connector wrap">
-	<h1><?php echo esc_html( $GLOBALS['title'] ); ?></h1>
+	<h1 class="wp-heading-inline"><?php echo esc_html( $GLOBALS['title'] ); ?></h1>
 
+    <hr class="wp-header-end">
 	<?php if ( isset( $_GET['settings-updated'] ) ) { ?>
 		<div id=”message” class="updated settings-error notice is-dismissible">
 			<p><strong><?php echo __( 'Settings saved.' ) ?></strong></p>
 		</div>
 	<?php } ?>
 
-	<div class="form-wrap">
-		<form method="post" action="options.php">
-			<?php wp_nonce_field( 'update-options' ); ?>
-			<table class="form-table">
+    <form method="post" action="options.php">
+        <?php wp_nonce_field( 'update-options' ); ?>
+        <table class="form-table">
 
-				<?php
-				settings_fields( 'smartcat' );
+            <?php
+                settings_fields( 'smartcat' );
 				do_settings_sections( 'smartcat' );
 				?>
-			</table>
+        </table>
 
-			<p class="submit">
-				<input type="submit" class="button-primary" value="<?php echo __( 'Save Changes' ) ?>"/>
-			</p>
-
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php echo __( 'Save Changes' ) ?>"/>
+        </p>
+        
 		</form>
-	</div>
 </div>
